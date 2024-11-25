b4fb763f67b95ba35a209a03bb06dcd842ddf7ff
volodout <volodout@gmail.com>
2024-11-25 09:38:45 +0500

    checksum + struct

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
 import argparse
 import os
+import hashlib
+import struct
 
 from huffman import huffman_encode, huffman_decode, save_codes, load_codes
 from utils import read_file, write_file, get_file_metadata, restore_file_metadata
 
 
-def bits_to_bytes(bits):
-    byte_array = bytearray()
-    for i in range(0, len(bits), 8):
-        byte = bits[i:i + 8]
-        if len(byte) < 8:
-            byte = byte.ljust(8, '0')
-        byte_array.append(int(byte, 2))
-    return byte_array
+def calculate_checksum(data):
+    return hashlib.sha256(data).digest()
 
 
-def bytes_to_bits(byte_data):
-    bits = ''
-    for byte in byte_data:
-        bits += bin(byte)[2:].rjust(8, '0')
-    return bits
-
-
-def compress_file(input_file, output_encoded_file, verbose: bool):
+def compress_file(input_file: str, output_encoded_file: str, verbose: bool):
     if output_encoded_file is None:
         output_encoded_file = input_file + '.huff'
     elif not output_encoded_file.endswith('.huff'):
         output_encoded_file += '.huff'
+
     data = read_file(input_file)
+    checksum = calculate_checksum(data)
+
     encoded_data, codes = huffman_encode(data)
 
-    byte_data = bits_to_bytes(encoded_data)
+    encoded_int = int(encoded_data, 2)
+    encoded_bytes = encoded_int.to_bytes((len(encoded_data) + 7) // 8, byteorder='big')
 
     with open(output_encoded_file, 'wb') as f:
-        length = len(byte_data)
-        f.write(length.to_bytes(4, byteorder='big'))
-        f.write(byte_data)
+        f.write(struct.pack('>I', len(encoded_data)))
+        f.write(checksum)
+        f.write(encoded_bytes)
         save_codes(codes, f)
 
     original_metadata = get_file_metadata(input_file)
@@ -50,21 +43,27 @@ def compress_file(input_file, output_encoded_file, verbose: bool):
         print(f"Файл сжат на {compression_ratio:.2f}%")
 
 
-def decompress_file(encoded_input_file, decoded_output_file):
+def decompress_file(encoded_input_file: str, decoded_output_file: str):
     if not decoded_output_file:
         decoded_output_file = encoded_input_file.replace('.huff', '')
     decoded_output_file = create_filename_to_decompress(decoded_output_file)
+
     with open(encoded_input_file, 'rb') as f:
-        length_bytes = f.read(4)
-        length = int.from_bytes(length_bytes, byteorder='big')
+        bit_length = struct.unpack('>I', f.read(4))[0]  # Длина битов
+        stored_checksum = f.read(32)  # Контрольная сумма
 
-        byte_data = f.read(length)
-
-        encoded_data = bytes_to_bits(byte_data)
+        encoded_bytes = f.read((bit_length + 7) // 8)
+        encoded_int = int.from_bytes(encoded_bytes, byteorder='big')
+        encoded_data = bin(encoded_int)[2:].zfill(bit_length)  # Преобразуем в строку бит
 
         codes = load_codes(f)
 
         decoded_data = huffman_decode(encoded_data, codes)
+
+        calculated_checksum = calculate_checksum(decoded_data)
+        if stored_checksum != calculated_checksum:
+            raise ValueError("Ошибка: контрольная сумма не совпадает. Файл повреждён!")
+
         write_file(decoded_output_file, decoded_data)
 
         original_metadata = get_file_metadata(encoded_input_file)
@@ -73,7 +72,7 @@ def decompress_file(encoded_input_file, decoded_output_file):
         print(f'Файл "{encoded_input_file}" успешно распакован в "{decoded_output_file}"')
 
 
-def create_filename_to_decompress(filename):
+def create_filename_to_decompress(filename: str):
     count = 1
     root, ext = os.path.splitext(filename)
     new_filename = root
@@ -113,11 +112,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# контроль целостности
-# прогресс бары +
-# тесты
-# архивировать несколько файлов
-# директории
-# -v процент сжатия
